fixed label value in adult dataset and removed normalization step from unlearning model and added a module for unlearning and new data injection

scripts/exp_outlier_enforced_iter.py
@@ -20,13 +20,13 @@ resultspath='/'.join(parts)+'/results/'
 modelpath='/'.join(parts)+'/models/'
 from experiment_utils import data_normalization, data_norm_log
 from unlearning.unlearn_eval import *
-from unlearning.unlearn_lvq import unlearn_sample_effect_glvq
-from utils import samples_unlearn_outliers, relearn_unlearn_samples
+from unlearning.unlearn_lvq import unlearn_sample_effect_glvq, unlearn_relearn_sample_glvq
+from utils import samples_unlearn_outliers, relearn_unlearn_samples, samples_enforce_random
 # || Dataset name: Breast cancer data ||
 from experiment_utils import dataset_health
 #dname='breastcancer'
 dname_all=['diabetes', 'surgical', 'banking', 'adult', 'criteo']
-dname=dname_all[2]
+dname=dname_all[1]
 Xtrain, Ytrain, Xtest, Ytest, features=dataset_health(dname)
 #zXtrain, zXtest=data_normalization(Xtrain, Xtest)
 zXtrain, zXtest=data_norm_log(Xtrain, Xtest)
@@ -94,6 +94,7 @@ for nprot in [1, 2, 3]:
                      'relearn_indices':relearn_indices, 'relearn_samples':relearn_samples}
         unlearn_indices,relearn_indices=outlier_learn_set['unlearn_indices'], outlier_learn_set['relearn_indices']
         unlearn_samples,relearn_samples=outlier_learn_set['unlearn_samples'], outlier_learn_set['relearn_samples']
+        enforce_indices, enforce_samples=samples_enforce_random(Xtrain, unlearn_indices,Ytrain)
         unlearn_labs,relearn_labs=Ytrain[unlearn_indices], Ytrain[relearn_indices]
         zXretrain=zXtrain.iloc[relearn_indices].copy()
         #Retraining 
@@ -122,27 +123,25 @@ for nprot in [1, 2, 3]:
             print('Appropriate step size for gradient ascent with %s is being searched using relearning set'%solver_type)
             for idx, grad_step in enumerate(grad_step_sizes):
                 glvq_copy.unlearn_rate_=grad_step
-                updated_model_attempt=unlearn_sample_effect_glvq(
-                        glvq_copy, zXtrain.iloc[unlearn_indices], unlearn_labs, training_info)
+                updated_model_attempt=unlearn_relearn_sample_glvq(glvq_copy, zXtrain.iloc[unlearn_indices], 
+                            unlearn_labs, zXtrain.iloc[enforce_indices], Ytrain[enforce_indices],training_info)
+               # updated_model_attempt.normalize_variables(updated_model_attempt.prototypes_)
                 # Compare original (0) vs retrained (1)
                 perf123=compare_perf_3(glvq, glvq_partial1, updated_model_attempt, data_dict, relearn_labs)
                 # ideal scenario: accuracy of retrained model (M1) should be less than that of unlearned model (M2) 
                 acc_diff[idx]=perf123['M0_Bacc']-perf123['M2_Bacc'] 
                 glvq_copy.prototypes_=glvq_copy.secure_prototypes_.copy()
             sorted_idx=np.argsort(acc_diff)
-    #       print('Appropriate step size for gradient ascent with %s is %.3f'%(solver_type, grad_step_sizes[sorted_idx[0]]))
             glvq_copy.unlearn_rate_=grad_step_sizes[sorted_idx[0]]
         # Unlearning of sample effects
         st_un=time.time()
-        unlearned_model=unlearn_sample_effect_glvq(glvq_copy, zXtrain.iloc[unlearn_indices], unlearn_labs, training_info)
-        #unlearned_iter[iter]={'model': unlearned_model, 'unlearn_indices': unlearn_indices }
+        unlearned_model=unlearn_relearn_sample_glvq(glvq_copy, zXtrain.iloc[unlearn_indices], unlearn_labs, 
+                                                     zXtrain.iloc[enforce_indices], Ytrain[enforce_indices],training_info)
         elapsed_untrain=(time.time()-st_un)/60
-    #       print('n=%d, Elapsed time diff=%3f-%3f'%(len(unlearn_indices), elapsed_retrain,elapsed_untrain))
         #########################################
         dev02, max_dev_indx02=compare_fidelity_glvq(glvq, unlearned_model)
-    #        print('After unlearning: Deviation between original and unlearned models:', dev02)
         dev12, max_dev_indx12=compare_fidelity_glvq(glvq_partial1, unlearned_model)
-    #        print('After unlearning: Deviation between retrained and unlearned models:', dev12)
+        print('After unlearning: Deviation between original and unlearned models:', dev02)
         ###############################################################################################################
         cratio_uo_ur=dev02/dev12
         print('Dev(prots from original and unlearned models)/Dev(prots from retrained and unlearned models)=%0.03f'%cratio_uo_ur)
@@ -181,12 +180,11 @@ for nprot in [1, 2, 3]:
             compare_df=pd.concat([compare_df, temp])
             cint+=1
         if cint>0:
-            tab_filename='%s%s/%s_outlier_unlearn_%s_nprot%d.csv'%(resultspath, dname, dname, solver_type, nprots_per_class)
-    #        print(tab_filename)
+            tab_filename='%s%s/%s_outlier_unlearn_enforce_%s_nprot%d.csv'%(resultspath, dname, dname, solver_type, nprots_per_class)
         compare_df.to_csv(tab_filename, index=False, sep='\t')
     if cint>0:
         model_sets={'original': glvq, 'retrained': retrained_n, 'unlearned': unlearned_n}
-        picklefilename='%s/%s/%s_outlier_%s_nprot%d.pkl'%(modelpath, dname, dname, solver_type, nprots_per_class)
+        picklefilename='%s/%s/%s_outlier_unlearn_enforce_%s_nprot%d.pkl'%(modelpath, dname, dname, solver_type, nprots_per_class)
         with open(picklefilename, 'wb') as file:
             pickle.dump(model_sets, file)
     print(dname, ' Outlier ', solver_type, ' Num prots: ', nprots_per_class)

scripts/exp_random_samples_mult_wgd.py
@@ -28,7 +28,7 @@ from experiment_utils import dataset_health
 #dname='breastcancer'
 #'adult' #'surgical' # 'diabetes'
 dname_all=['diabetes', 'surgical', 'banking', 'adult', 'criteo']
-dname=dname_all[4]
+dname=dname_all[1]
 Xtrain, Ytrain, Xtest, Ytest, features=dataset_health(dname)
 #zXtrain, zXtest=data_normalization(Xtrain, Xtest)
 zXtrain, zXtest=data_norm_log(Xtrain, Xtest)
@@ -44,7 +44,7 @@ else:
 # Model params to compare; 
 # Training original model
 dist_name, activation_type="squared-euclidean", "identity"
-solver_type, solver_params="lbfgs", {"max_runs": 5, "step_size": np.array([0.05]),  "k": 3,
+solver_type, solver_params="sgd", {"max_runs": 5, "step_size": np.array([0.05]),  #"k": 3,
 }
 print(dname, ' random ', solver_type)
 for nprots in [1,2,3]:

scripts/unlearning/unlearn_eval.py
@@ -11,7 +11,7 @@ def compare_fidelity_glvq(model1, model2):
     and returns the root mean squared error of their differences, and the 
     sorted indices of the features based on the absolute deviations
     """
-    deviation=np.round(rmse(model1.prototypes_,model2.prototypes_), 5)
+    deviation=rmse(model1.prototypes_,model2.prototypes_)
     max_dev_indx=np.argsort(np.abs(model1.prototypes_-model2.prototypes_), axis=1)
     return deviation, max_dev_indx
 

scripts/unlearning/unlearn_lvq.py
@@ -10,12 +10,22 @@ if parts[-1]=='notebooks':
 code_path='/'.join(parts)+'/scripts/'
 sys.path.append(code_path)
 from utils import compute_distances
+from collections import Counter
 
 def unlearn_sample_effect_glvq(model:sklvq.models._glvq.GLVQ, unlearn_data:pd.DataFrame, 
                                unlearn_labels:list, training_info:dict):
     """
+    The sklvq toolbox based trained GLVQ model `model` along with the data to be unlearned, `unlearn_data`,
+    and their respective labels (`unlearn_labels` ) are passed into this function.The dictionary `training_info`
+    contains information about the original training set size and the class strengths of the classes it trained on. 
+
+    The function returns the updated model with the effects of the `adapt_data` removed.
+    
+    Note: This function has been evaluated for the solver types `lbfgs`, `wgd` and `sgd` (steepest gradient descent`, where learning 
+    took place in a single batch. 
+    We do not know yet whether unlearning from a model trained via a stochastic gradient descent or mini batch gradient descent would 
+    work following the same scheme as ordering of samples play a crucial role in these. 
     """
-    #data_unlearn=zXtrain.iloc[unlearn_indices].copy()
     if model.solver_type=='wgd':
         k=model.get_params()['solver_params']['k']
         max_iter=k
@@ -25,55 +35,212 @@ def unlearn_sample_effect_glvq(model:sklvq.models._glvq.GLVQ, unlearn_data:pd.Da
         max_runs=model.get_params()['solver_params']['max_runs']
         max_iter=max_runs
     else:
-        max_iter=3
-    max_iter=1
-    for iter in range(0, max_iter):
-        dist_same, dist_diff, i_dist_same, i_dist_diff=compute_distances(model, unlearn_data, unlearn_labels)
-        updated_prots=model.prototypes_.copy()
+        max_iter=1
+    max_iter, new_N, old_N=1, training_info['setsize']-len(unlearn_labels), training_info['setsize']
+    secure_copy=model.prototypes_.copy()
+    dist_same, dist_diff, i_dist_same, i_dist_diff=compute_distances(model, unlearn_data, unlearn_labels)
+    updated_prots=model.prototypes_.copy()
+    for iter in range(0, max_iter):   
         for nprot in range(0,model.prototypes_labels_.shape[0]):
-            prot=np.reshape(model.prototypes_[nprot], (1,model.prototypes_.shape[1]))
-            if iter==0:
-                initial_cw=training_info['class_weight'][model.prototypes_labels_[nprot]]
-                cw_new=initial_cw-np.sum(unlearn_labels==nprot)
-                if np.sum(unlearn_labels==nprot)>1:
-                    prot_init_corr=prot-(prot*initial_cw-unlearn_data.iloc[unlearn_labels==nprot].sum(skipna=True).to_numpy())/cw_new
-                elif np.sum(unlearn_labels==nprot)==1:
-                    prot_init_corr=prot-(prot*initial_cw-unlearn_data.iloc[unlearn_labels==nprot].to_numpy())/cw_new
-                else:
-                    prot_init_corr=prot*0
-                prot_init_corr=np.reshape(prot_init_corr, (1,model.prototypes_.shape[1]))
-                updated_prots[nprot,:]=updated_prots[nprot,:]-prot_init_corr
-                #np.reshape(unlearn_data.iloc[].mean(), (1,model.prototypes_.shape[1]))
+            prot_lab=model.prototypes_labels_[nprot]
+           # print(prot_lab, training_info['class_weight'])
+            initial_cw=training_info['class_weight'][prot_lab]
+            cw_new=initial_cw-np.sum(unlearn_labels==prot_lab)
+            prot=np.reshape(model.prototypes_[nprot].copy(), (1,model.prototypes_.shape[1]))
             idx_same, idx_diff=np.where(i_dist_same==nprot)[0], np.where(i_dist_diff==nprot)[0]
             get_grad_same=sklvq.distances.SquaredEuclidean.gradient(sklvq.distances.SquaredEuclidean(), 
                                                                    data=unlearn_data.iloc[idx_same], model=model, i_prototype=nprot)
             get_grad_diff=sklvq.distances.SquaredEuclidean.gradient(sklvq.distances.SquaredEuclidean(), 
                                                                    data=unlearn_data.iloc[idx_diff], model=model, i_prototype=nprot)
+            if iter==0:
+                if np.sum(unlearn_labels==prot_lab)>1:
+                    prot_init_corr=(prot*initial_cw-unlearn_data.loc[unlearn_labels==prot_lab].sum(skipna=True).to_numpy())/cw_new
+                    #prot_init_corr=prot-unlearn_data.loc[unlearn_labels==prot_lab].mean(skipna=True).to_numpy()#/cw_new
+                elif np.sum(unlearn_labels==prot_lab)==1:
+                    prot_init_corr=(prot*initial_cw-unlearn_data.loc[unlearn_labels==prot_lab].to_numpy())/cw_new
+                else:
+                    prot_init_corr=model.prototypes_[nprot].copy()*0#*initial_cw/cw_new
+                prot_init_corr=np.reshape(prot_init_corr, (1,model.prototypes_.shape[1]))
+                #updated_prots[nprot]=updated_prots[nprot]-prot_init_corr[0]
+                updated_prots[nprot]=prot_init_corr[0]#/cw_new
             if len(idx_same)>0:
                 if len(idx_same)==1:
                     unlearn_grad_same=get_grad_same*unlearn_data.iloc[idx_same]
-                    prot_init_corr=np.reshape(unlearn_data.iloc[idx_same], (1,model.prototypes_.shape[1]))
+                    unlearn_grad_same_sum=unlearn_grad_same.copy()
                 else:
-                    unlearn_grad_same=get_grad_same.mean()*unlearn_data.iloc[idx_same].mean(skipna=True)
-                    prot_init_corr=np.reshape(unlearn_data.iloc[idx_same].mean(skipna=True), (1,model.prototypes_.shape[1]))
+                    unlearn_grad_same=get_grad_same.mean(skipna=True)*unlearn_data.iloc[idx_same].mean(skipna=True)
+                    unlearn_grad_dummy_same=get_grad_same*unlearn_data.iloc[idx_same]
+                    unlearn_grad_same_sum=np.sum(unlearn_grad_dummy_same, axis=0)
+                    del  unlearn_grad_dummy_same
             else:
+                unlearn_grad_same_sum=np.zeros(np.shape(prot))
                 unlearn_grad_same=np.zeros(np.shape(prot))
             if len(idx_diff)>0:
                 if len(idx_diff)==1:
                     unlearn_grad_diff=get_grad_diff*unlearn_data.iloc[idx_diff]
-                   # prot_init_corr=np.reshape(unlearn_data.iloc[idx_diff], (1,model.prototypes_.shape[1]))
+                    unlearn_grad_diff_sum=unlearn_grad_diff.copy()
                 else:
-                    unlearn_grad_diff=get_grad_diff.mean()*unlearn_data.iloc[idx_diff].mean(skipna=True)
-                 #   prot_init_corr=np.reshape(unlearn_data.iloc[idx_diff].mean(), (1,model.prototypes_.shape[1]))
+                    unlearn_grad_diff=get_grad_diff.mean(skipna=True)*unlearn_data.iloc[idx_diff].mean(skipna=True)
+                    unlearn_grad_dummy_diff=get_grad_diff*unlearn_data.iloc[idx_diff]
+                    unlearn_grad_diff_sum=np.sum(unlearn_grad_dummy_diff, axis=0)
+                    del  unlearn_grad_dummy_diff
             else:
+                unlearn_grad_diff_sum=np.zeros(np.shape(prot))
                 unlearn_grad_diff=np.zeros(np.shape(prot))
             unlearn_grad_diff=np.reshape(unlearn_grad_diff, (1,model.prototypes_.shape[1]))
             unlearn_grad_same=np.reshape(unlearn_grad_same, (1,model.prototypes_.shape[1]))
+            #unlearn_grad_diff_sum=np.reshape(unlearn_grad_diff_sum, (1,model.prototypes_.shape[1]))
+            #unlearn_grad_same_sum=np.reshape(unlearn_grad_same_sum, (1,model.prototypes_.shape[1]))
             if model.get_params()['solver_type'] in ["sgd", "wgd"]:
-                updated_prots[nprot,:]=updated_prots[nprot,:]+(unlearn_grad_diff-unlearn_grad_same)*model.get_params()['solver_params']['step_size'][0]
+                updated_prots[nprot]=(updated_prots[nprot]-(unlearn_grad_diff-
+                                                            unlearn_grad_same)*model.get_params()['solver_params']['step_size'][0])*(old_N/new_N)
             else:
-                updated_prots[nprot,:]=updated_prots[nprot,:]+(unlearn_grad_diff-unlearn_grad_same)*model.unlearn_rate_
-        model.set_prototypes(updated_prots)
-        model.normalize_variables(model.prototypes_)
+           #    updated_prots[nprot]=(updated_prots[nprot]*initial_cw-(unlearn_grad_diff_sum-
+           #                                                                 unlearn_grad_same_sum)*model.unlearn_rate_)/cw_new#
+                updated_prots[nprot]=(updated_prots[nprot]-(unlearn_grad_diff-
+                                                                            unlearn_grad_same)*model.unlearn_rate_)*(old_N/new_N)
+            del unlearn_grad_diff, unlearn_grad_same, unlearn_grad_diff_sum, unlearn_grad_same_sum
+            del prot, prot_init_corr, cw_new, initial_cw
+    model.set_prototypes(updated_prots)
+    #model.normalize_variables(model.prototypes_)
     return model
+
+    
+def adapt_sample_effect_glvq(model:sklvq.models._glvq.GLVQ, adapt_data:pd.DataFrame, 
+                               adapt_labels:list, adapt_type:str, training_info:dict):
+    """
+    The sklvq toolbox based trained GLVQ model `model` along with the data to be adapted (unlearned or injected), `adapt_data`,
+    and their respective labels (`adapt_labels` ) are passed into the function. The variable `adapt_type` tells whether the adaption
+    should be unlearning or injection of the `adapt_data`. The dictionary `training_info` contains information about the original 
+    training set size and the class strengths of the classes it trained on. 
+
+    The function returns the updated model with the effects of the `adapt_data` removed from injected, based on `adapt_type`.
+
+    Note: This function has been evaluated for the solver types `lbfgs`, `wgd` and `sgd` (steepest gradient descent`, where learning 
+    took place in a single batch. 
+    We do not know yet whether unlearning from a model trained via a stochastic gradient descent or mini batch gradient descent would 
+    work following the same scheme as ordering of samples play a crucial role in these. 
+    """
+    max_iter=1
+    if adapt_type=='unlearn':
+        adapt_factor=1
+    else:
+        adapt_factor=-1
+    new_N, old_N=training_info['setsize']-len(adapt_labels), training_info['setsize']
+    secure_copy=model.prototypes_.copy()
+    dist_same, dist_diff, i_dist_same, i_dist_diff=compute_distances(model, adapt_data, adapt_labels)
+    updated_prots=model.prototypes_.copy()
+    for iter in range(0, max_iter):   
+        for nprot in range(0,model.prototypes_labels_.shape[0]):
+            prot_lab=model.prototypes_labels_[nprot]
+            initial_cw=training_info['class_weight'][prot_lab]
+            cw_new=initial_cw-np.sum(adapt_labels==prot_lab)
+            prot=np.reshape(model.prototypes_[nprot].copy(), (1,model.prototypes_.shape[1]))
+            idx_same, idx_diff=np.where(i_dist_same==nprot)[0], np.where(i_dist_diff==nprot)[0]
+            get_grad_same=sklvq.distances.SquaredEuclidean.gradient(sklvq.distances.SquaredEuclidean(), 
+                                                                   data=adapt_data.iloc[idx_same], model=model, i_prototype=nprot)
+            get_grad_diff=sklvq.distances.SquaredEuclidean.gradient(sklvq.distances.SquaredEuclidean(), 
+                                                                   data=adapt_data.iloc[idx_diff], model=model, i_prototype=nprot)
+        # Approximate difference between initialized prots from original data (class-conditional means) 
+        # and conditional means from adapt_data. The difference is then adjusted 
+        # (subtracted for unlearning, added for relearning) in the trained model's prototype.
+            if iter==0: 
+                if np.sum(adapt_labels==prot_lab)>1:
+                    prot_init_corr=(prot*initial_cw-adapt_factor*adapt_data.loc[adapt_labels==prot_lab].sum(skipna=True).to_numpy())/cw_new
+                elif np.sum(adapt_labels==prot_lab)==1:
+                    prot_init_corr=(prot*initial_cw-adapt_factor*adapt_data.loc[adapt_labels==prot_lab].to_numpy())/cw_new
+                else:
+                    prot_init_corr=model.prototypes_[nprot].copy()*0
+                prot_init_corr=np.reshape(prot_init_corr, (1,model.prototypes_.shape[1]))
+                updated_prots[nprot]=prot_init_corr[0]
+            if len(idx_same)>0:
+                if len(idx_same)==1:
+                    adapt_grad_same=get_grad_same*adapt_data.iloc[idx_same]
+                    adapt_grad_same_sum=adapt_grad_same.copy()
+                else:
+                    adapt_grad_same=get_grad_same.mean(skipna=True)*adapt_data.iloc[idx_same].mean(skipna=True)
+                    adapt_grad_dummy_same=get_grad_same*adapt_data.iloc[idx_same]
+                    adapt_grad_same_sum=np.sum(adapt_grad_dummy_same, axis=0)
+                    del  adapt_grad_dummy_same
+            else:
+                adapt_grad_same_sum,adapt_same=np.zeros(np.shape(prot)), np.zeros(np.shape(prot))
+            if len(idx_diff)>0:
+                if len(idx_diff)==1:
+                    adapt_grad_diff=get_grad_diff*adapt_data.iloc[idx_diff]
+                    adapt_grad_diff_sum=adapt_grad_diff.copy()
+                else:
+                    adapt_grad_diff=get_grad_diff.mean(skipna=True)*adapt_data.iloc[idx_diff].mean(skipna=True)
+                    adapt_grad_dummy_diff=get_grad_diff*adapt_data.iloc[idx_diff]
+                    adapt_grad_diff_sum=np.sum(adapt_grad_dummy_diff, axis=0)
+                    del adapt_grad_dummy_diff
+            else:
+                adapt_grad_diff_sum=np.zeros(np.shape(prot))
+                adapt_grad_diff=np.zeros(np.shape(prot))
+            adapt_grad_diff=np.reshape(adapt_grad_diff, (1,model.prototypes_.shape[1]))
+            adapt_grad_same=np.reshape(adapt_grad_same, (1,model.prototypes_.shape[1]))
+            if model.get_params()['solver_type'] in ["sgd", "wgd"]:
+                updated_prots[nprot]=(updated_prots[nprot]-adapt_factor*(adapt_grad_diff-
+                                                            adapt_grad_same)*model.get_params()['solver_params']['step_size'][0])*(old_N/new_N)
+            else:
+                updated_prots[nprot]=(updated_prots[nprot]-adapt_factor*(adapt_grad_diff-
+                                                                            adapt_grad_same)*model.unlearn_rate_)*(old_N/new_N)
+            del adapt_grad_diff, adapt_grad_same, adapt_grad_diff_sum, adapt_grad_same_sum
+            del prot, prot_init_corr, cw_new, initial_cw
+    model.set_prototypes(updated_prots)
+    return model
+
+def unlearn_relearn_sample_glvq(model:sklvq.models._glvq.GLVQ, data_dict: dict, label_dict: dict,
+                                adapt_action:list, training_info:dict):
+    """
+    Samples to be unlearned and / or injected are passed into this function in `data_dict` together with their respective label
+    in `label_dict`, following the naming convention: `unlearn` or `inject` preceeding `_data` or `_labels` in the respective 
+    dictionaries, based on the action that is required to be performed. The `adapt_action` argument is passed as a list of string
+    with action name `unlearn` and/or `inject`.
+
+    The function returns an updated model based on unlearning of specified data or injection of new data or data whose effects are 
+    to be enforced, or both. It also returns a container with the changed statistics of the data used in the original training, 
+    unlearning and/or injection (`cont_stats_change`), where the 0th key-value pair correspond to the original training data based
+    statistic.
+    """
+    c, cont_stats_change=0, {}
+    if len(adapt_action)>1:
+        for seq in adapt_action:
+            adapt_data, adapt_labels=data_dict['%s_data'%seq], label_dict['%s_labels'%seq]
+            adapt_type, old_N=seq, training_info['setsize']
+            cont_stats_change[c]={'change_type':None,'stats': training_info}
+            if adapt_type=='unlearn':
+                old_adapt_fac=-1
+            else:
+                old_adapt_fac=1
+            cw_dist0,cw_adapt0 =training_info['class_weight'], Counter(adapt_labels)
+            for lab in cw_adapt0.keys():
+                cw_dist0[lab]=cw_dist0[lab]+old_adapt_fac*len(adapt_labels)
+            if c==0:
+                updated_model=adapt_sample_effect_glvq(model, adapt_data, adapt_labels, adapt_type, training_info)
+            else:
+                updated_model=adapt_sample_effect_glvq(updated_model, adapt_data, adapt_labels, adapt_type, training_info)
+            training_info['setsize']=old_N+old_adapt_fac*len(adapt_labels)
+            training_info['class_weight']=cw_dist0.copy()
+            c+=1
+            cont_stats_change[c]={'change_type':adapt_type,'stats': training_info}
+    else:
+        adapt_type=adapt_action[0]
+        if adapt_type=='unlearn':
+            old_adapt_fac=-1
+        else:
+            old_adapt_fac=1
+        adapt_data, adapt_labels=data_dict['%s_data'%adapt_type], label_dict['%s_labels'%adapt_type]
+        updated_model=adapt_sample_effect_glvq(model, adapt_data, adapt_labels, adapt_type, training_info)
+        cw_dist0, cw_adapt0 =training_info['class_weight'], Counter(adapt_labels)
+        for lab in cw_adapt0.keys():
+            cw_dist0[lab]=cw_dist0[lab]+old_adapt_fac*len(adapt_labels)
+        
+        training_info['setsize']=training_info['setsize']+old_adapt_fac*len(adapt_labels)
+        training_info['class_weight']=cw_dist0.copy()
+        cont_stats_change[c]={'change_type':adapt_type,'stats': training_info}
+        return updated_model, cont_stats_change
+    
+        
+                
+            
     

scripts/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import os, sys
 import sklvq
 import pandas as pd
+import itertools
 
 parts=os.getcwd().split('/')
 if parts[-1]=='notebooks':
@@ -56,8 +57,11 @@ def samples_unlearn_random(trainset:pd.DataFrame,trainlabs:list, n:int=20,save_s
     unlearn_indices=np.random.randint(0, trainset.shape[0], size=n).tolist()
     unlearn_samples=trainset.iloc[unlearn_indices].copy()
     relearn_indices, relearn_samples=relearn_unlearn_samples(trainset, unlearn_indices,save_samples)
+ #   enforce_indices, enforce_samples=samples_enforce_random(trainset, unlearn_indices,trainlabs)
     unlearn_set={'unlearn_indices':unlearn_indices, 'unlearn_samples':unlearn_samples, 'unlearn_labs':trainlabs[unlearn_indices],
-                 'relearn_indices':relearn_indices, 'relearn_samples':relearn_samples, 'relearn_labs':trainlabs[relearn_indices]}
+                 'relearn_indices':relearn_indices, 'relearn_samples':relearn_samples, 'relearn_labs':trainlabs[relearn_indices],
+                # 'enforce_indices':enforce_indices, 'enforce_samples':enforce_samples, 'enforce_labs':trainlabs[relearn_indices]
+                }
     if save_samples==1:
         unlearn_samples.insert(0,'og_index',unlearn_indices)
         unlearn_samples.to_csv(datapath+'unlearn_random_samples_%d.csv'%n, index=False)
@@ -83,6 +87,39 @@ def samples_unlearn_outliers(trainset:pd.DataFrame,model:sklvq.models, thresh:fl
         unlearn_samples.to_csv(datapath+'unlearn_outlier_samples.csv', index=False)
     return unlearn_set
 
+
+def samples_enforce_random(trainset:pd.DataFrame, unlearn_indices:list, trainlabs:list):
+    """
+    """
+    nlabs=np.unique(trainlabs[unlearn_indices])
+    if len(unlearn_indices)==1:
+        searchset_indices=np.setdiff1d(list(np.where(trainlabs==trainlabs[unlearn_indices])[0]), unlearn_indices)
+        #trainset.loc[trainlabs==trainlabs[unlearn_indices]].copy()
+        enforce_indices=searchset_indices[np.random.randint(0, len(searchset_indices), size=1).tolist()]
+    elif len(nlabs)==1:
+        n=len(unlearn_indices)
+        searchset_indices=np.setdiff1d(list(np.where(trainlabs==nlabs)[0]), unlearn_indices)
+        within_class_sel=list(np.random.randint(0, len(searchset_indices), size=n))
+        enforce_indices=searchset_indices[within_class_sel]
+    else:
+        #intializing container to store indices to enforce
+        enforce_indices=[]
+        for nlab in nlabs:
+            n=np.sum(trainlabs[unlearn_indices]==nlab)
+            searchset_indices=np.setdiff1d(list(np.where(trainlabs==nlab)[0]), unlearn_indices)
+            if len(searchset_indices)==1:
+                within_class_sel=list(np.random.randint(0, len(searchset_indices), size=1))
+            elif len(searchset_indices)<=n:
+                n=len(searchset_indices)
+                within_class_sel=list(np.random.randint(0, len(searchset_indices), size=n))
+            else:
+                within_class_sel=list(np.random.randint(0, len(searchset_indices), size=n))
+            perlab_indices=searchset_indices[within_class_sel]#.tolist()        
+            enforce_indices.append(perlab_indices)
+        enforce_indices=list(itertools.chain(*enforce_indices))
+    return enforce_indices, trainset.iloc[enforce_indices]
+    
+    
 
 def relearn_unlearn_samples(trainset:pd.DataFrame, unlearn_indices,save_samples:int=0):
     """